8ffed4e68db4b7a7742e49a1d470b504a0395377
Doug Stanley <DouglasStanley@outlook.com>
2024-01-14 15:02:02 -0800

    Add branch endpoint

--- msvc1/Program.cs
+++ msvc1/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,5 +20,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/branch", () =>
+{
+    return GitVersionInformation.FullSemVer;
+});
 
 app.Run();
